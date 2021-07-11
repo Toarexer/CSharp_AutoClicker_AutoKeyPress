ed5f3b504914cea8e6fb2cfd39f52c00ff37a71a
Toarexer <58662537+Toarexer@users.noreply.github.com>
2021-07-11 16:18:13 +0200

    Process Search Bar
    Added search bar to the process list and removed a few unnecessary things.

--- AutoClicker/AutoClicker/ListForm.cs
+++ AutoClicker/AutoClicker/ListForm.cs
@@ -5,23 +5,9 @@ using System.Windows.Forms;
 
 namespace AutoClicker
 {
-    class ProcessListBox : ListBox
-    {
-        // hidden scrollbar exaple form https://stackoverflow.com/questions/13169900/hide-vertical-scroll-bar-in-listbox-control
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams createParams = base.CreateParams;
-                //createParams.Style &= ~0x200000;
-                return createParams;
-            }
-        }
-    }
-
     class ListForm : Form
     {
-        ProcessListBox processListBox;
+        ListBox processListBox;
         Process[] processes;
 
         public ListForm()
@@ -33,22 +19,31 @@ namespace AutoClicker
 
             // Initialize Components
 
-            //Font font = new Font(FontFamily.GenericMonospace, 8, FontStyle.Bold);
             Font font = new Font("Consolas", 8f);
-            Font fontb = new Font("Consolas", 8f, FontStyle.Bold);
+            Font fontb = new Font(font, FontStyle.Bold);
 
             Label processListLabel = new Label();
-            processListLabel.Size = new Size(Width - 34, 16);
-            processListLabel.Location = new Point(0, 0);
+            processListLabel.Size = new Size(128, 20);
             processListLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             processListLabel.Text = "PID     |    Name";
             processListLabel.TextAlign = ContentAlignment.MiddleLeft;
             processListLabel.Font = fontb;
 
-            processListBox = new ProcessListBox();
+            TextBox processSearchTextBox = new TextBox();
+            processSearchTextBox.Size = new Size(192, 16);
+            processSearchTextBox.Dock = DockStyle.Right;
+            processSearchTextBox.TabIndex = 2;
+            processSearchTextBox.Text = "Search";
+            processSearchTextBox.ForeColor = Color.Gray;
+            processSearchTextBox.KeyDown += ListMatchingElements;
+            processSearchTextBox.KeyPress += (s, e) => FormatProcessSearchTextBox(s, true);
+            processSearchTextBox.LostFocus += (s, e) => FormatProcessSearchTextBox(s, false);
+
+            processListBox = new ListBox();
             processListBox.IntegralHeight = true;
-            processListBox.Size = new Size(Width - 16, Height - 48);
-            processListBox.Location = new Point(1, 16);
+            processListBox.Size = new Size(Width - 16, Height - 56);
+            processListBox.Location = new Point(1, 20);
+            processListBox.TabIndex = 1;
             processListBox.ScrollAlwaysVisible = true;
             processListBox.SelectionMode = SelectionMode.One;
             processListBox.MouseDoubleClick += OnSelected;
@@ -56,8 +51,9 @@ namespace AutoClicker
             processListBox.Font = font;
 
             Controls.Add(processListLabel);
+            Controls.Add(processSearchTextBox);
             Controls.Add(processListBox);
-            
+
             // Fill up the ListBox
 
             processes = Process.GetProcesses();
@@ -66,9 +62,9 @@ namespace AutoClicker
         }
 
         void OnResized(object sender, EventArgs e)
-        {   
+        {
             processListBox.Width = Width - 16;
-            processListBox.Height = Height - 48;
+            processListBox.Height = Height - 56;
         }
 
         void OnSelected(object sender, EventArgs e)
@@ -87,6 +83,35 @@ namespace AutoClicker
         {
             if (e.KeyCode == Keys.Enter)
                 OnSelected(sender, e);
+        }
+
+        void ListMatchingElements(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter || ((TextBox)sender).ForeColor == Color.Gray)
+                return;
+
+            processListBox.Items.Clear();
+            processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+                if (processes[i].ProcessName.ToLower().Contains(((TextBox)sender).Text.ToLower()))
+                    processListBox.Items.Add($"{processes[i].Id}\t|    {processes[i].ProcessName}");
+        }
+
+        void FormatProcessSearchTextBox(object sender, bool focused)
+        {
+            if (focused)
+            {
+                if (((TextBox)sender).Text == "Search")
+                {
+                    ((TextBox)sender).ForeColor = Color.Black;
+                    ((TextBox)sender).Text = "";
+                }
+            }
+            else if (((TextBox)sender).Text.Length == 0)
+            {
+                ((TextBox)sender).ForeColor = Color.Gray;
+                ((TextBox)sender).Text = "Search";
+            }
         }
     }
 }
